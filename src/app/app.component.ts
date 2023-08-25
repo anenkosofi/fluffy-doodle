c5cfa235a061a3a2c3dd411870f63c7cfa368373
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-08-25 08:57:38 +0300

    init: start ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'fluffy-doodle';
+  public username = '';
+  public displayDetails = false;
+  public logs: number[] = [];
+  public counter = 0;
+
+  onResetUsername() {
+    this.username = '';
+  }
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.counter++;
+    this.logs.push(this.counter);
+  }
 }
